5c238aa72957cc31c6083b7afdb86166eeddfcd5
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2023-11-19 13:10:14 -0300

    criando testes para os mixins

--- lib/utils/mixins/convert_file.dart
+++ lib/utils/mixins/convert_file.dart
@@ -16,17 +16,8 @@ mixin FormatFileMixin {
   }
 
   Uint8List extractDecodeBitAndConvertToUint8List(String base64Image) {
-    try {
-      // Encontra o índice da vírgula que separa o tipo de imagem e a string codificada em Base64
-      final commaIndex = base64Image.indexOf(',');
-
-      // Extrai a parte codificada em Base64 da string
-      final base64EncodedData = base64Image.substring(commaIndex + 1);
-
-      // Decodifica a string codificada em Base64 para uma lista Uint8List
-      return base64Decode(base64EncodedData);
-    } catch (e) {
-      return Uint8List(0);
-    }
+    final commaIndex = base64Image.indexOf(',');
+    final base64EncodedData = base64Image.substring(commaIndex + 1);
+    return base64Decode(base64EncodedData);
   }
 }
