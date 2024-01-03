47df4621d2f2b0e155de5472ea92052b6e88ccf7
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2024-01-03 19:39:59 -0300

    adicionando compartilhamento de arquivos

--- lib/pages/resumo_leituras_page/resumo_leituras_page.dart
+++ lib/pages/resumo_leituras_page/resumo_leituras_page.dart
@@ -1,10 +1,74 @@
+import 'dart:developer';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:my_light_app/enterprise/entities/leitura_resumo_entity.dart';
 import 'package:my_light_app/enterprise/usecases/get_leitura_resumo_usecase.dart';
 import 'package:my_light_app/utils/mixins/date_formate.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:share_extend/share_extend.dart';
 
-class ResumoLeiturasPage extends StatelessWidget with DateFormatMixin {
+//OpenFile.open("/sdcard/example.txt", type: "text/plain", uti: "public.plain-text");
+class ResumoLeiturasPage extends StatefulWidget with DateFormatMixin {
   final GetLeituraResumoUseCase getLeituraResumoUseCase;
   const ResumoLeiturasPage({super.key, required this.getLeituraResumoUseCase});
+
+  @override
+  State<ResumoLeiturasPage> createState() => _ResumoLeiturasPageState();
+}
+
+class _ResumoLeiturasPageState extends State<ResumoLeiturasPage> {
+  final resumoLeituras = ValueNotifier(<LeiturasResumoEntity>[]);
+  late Future<List<LeiturasResumoEntity>> leiturasLoading;
+  Future<void> gerarEPdf(List<LeiturasResumoEntity> leituras) async {
+    final pdf = pw.Document(deflate: zlib.encode);
+    pdf.addPage(
+      pw.MultiPage(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return [
+            pw.TableHelper.fromTextArray(
+              context: context,
+              data: <List<String>>[
+                <String>['Total de Leituras', 'Consumo', 'Data da Leitura'],
+                ...leituras.map((item) => [
+                      item.totalLeitura.toString(),
+                      item.consumo.toString(),
+                      widget.dateFormatStringMMYYYY(item.dataLeitura)
+                    ]),
+              ],
+            ),
+          ];
+        },
+      ),
+    );
+
+    final output = await getApplicationDocumentsDirectory();
+    final directoryPath = output.path;
+
+    final file = File("$directoryPath/leituras_resumo.pdf");
+    await file.writeAsBytes(await pdf.save());
+
+    log(file.path);
+    try {
+      if (await file.exists()) {
+        await ShareExtend.share(file.path, 'file', extraText: 'Enviar PDF');
+      } else {
+        log('File not found');
+      }
+    } catch (e) {
+      log('Erro ao abrir o arquivo: $e');
+    }
+  }
+
+  @override
+  void initState() {
+    final loading = widget.getLeituraResumoUseCase.exec();
+    leiturasLoading = loading;
+    loading.then((value) => resumoLeituras.value = value);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +76,27 @@ class ResumoLeiturasPage extends StatelessWidget with DateFormatMixin {
       appBar: AppBar(
         title: const Text('Resumo anual'),
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.download),
+          ValueListenableBuilder(
+            valueListenable: resumoLeituras,
+            builder: (context, resumo, snapshot) {
+              if (resumo.isNotEmpty) {
+                return IconButton(
+                  onPressed: () {
+                    gerarEPdf(resumo);
+                  },
+                  icon: const Icon(Icons.share),
+                );
+              }
+              return const SizedBox();
+            },
           )
         ],
       ),
       body: FutureBuilder(
-        future: getLeituraResumoUseCase.exec(),
+        future: leiturasLoading,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             final resumo = snapshot.data!;
-
             return SingleChildScrollView(
               child: DataTable(
                 columns: const [
@@ -39,7 +112,8 @@ class ResumoLeiturasPage extends StatelessWidget with DateFormatMixin {
                           DataCell(Text("${leitura.consumo} kwh")),
                           DataCell(
                             Text(
-                              dateFormatStringMMYYYY(leitura.dataLeitura),
+                              widget
+                                  .dateFormatStringMMYYYY(leitura.dataLeitura),
                             ),
                           ),
                         ],
